555ecf706c95fec3a7ccfcf0c3c5ce9b917a7e88
bny <eymen_15@hotmail.com>
2022-11-08 15:56:07 +0300

    phase_51

--- Week_6/Assets/Demo_1/Scripts/Player/PlayerCubes/PlayerCubeManager.cs
+++ Week_6/Assets/Demo_1/Scripts/Player/PlayerCubes/PlayerCubeManager.cs
@@ -9,6 +9,7 @@ public class PlayerCubeManager : MonoBehaviour
 {
     private Vector3 FirstCubePosition = Vector3.zero;
     private float stepLength = 0.043f;
+    private float playerValue = 0.0226f;
 
     public List<CubeBehaviour> listOfCubeBehaviour = new List<CubeBehaviour>();
 
@@ -51,7 +52,7 @@ public class PlayerCubeManager : MonoBehaviour
 
 
         var playerTransform = PlayerBehaviour.Instance.transform;
-        Vector3 playerTarget = new Vector3(0f, (indexOfCube) * stepLength + 0.0226f, 0f);
+        Vector3 playerTarget = new Vector3(0f, (indexOfCube) * stepLength + playerValue, 0f);
         playerTransform.DOLocalMove(playerTarget, 0.05f);
 
 
@@ -72,7 +73,7 @@ public class PlayerCubeManager : MonoBehaviour
         int indexOfCube = listOfCubeBehaviour.Count - 1;
 
         var playerTransform = PlayerBehaviour.Instance.transform;
-        Vector3 playerTarget = new Vector3(0f, (indexOfCube) * stepLength + 0.0226f, 0f);
+        Vector3 playerTarget = new Vector3(0f, (indexOfCube) * stepLength + playerValue, 0f);
         playerTransform.DOLocalMove(playerTarget, 0.05f);
 
     }
@@ -86,7 +87,7 @@ public class PlayerCubeManager : MonoBehaviour
         foreach (var cube in listOfCubeBehaviour)
         {
             Vector3 target = new Vector3(0f, index * stepLength, 0f);
-            cube.transform.DOLocalMove(target, 0.15f);
+            cube.transform.DOLocalMove(target, 0.05f);
             index--;
         }
     }
