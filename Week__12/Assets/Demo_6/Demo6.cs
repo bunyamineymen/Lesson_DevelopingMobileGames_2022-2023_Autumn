4d86ba35067c78778fa8b007a5418527b984a1c0
bny <eymen_15@hotmail.com>
2022-12-13 15:42:47 +0300

    phase_77

---
+++ Week__12/Assets/Demo_6/Demo6.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Demo6 : MonoBehaviour
+{
+    public Animator animatorController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        jumpControl();
+    }
+
+    void jumpControl()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            animatorController.SetTrigger("jump");
+
+
+    }
+}
